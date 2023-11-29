396d96af383e46cd067a462727d28682deed2d62
johnkjrfc <johnkjrfc@gmail.com>
2023-11-29 14:43:29 -0600

    command class complete

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,16 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  it("constructor sets command type" ,function() {
+
+    let command = new Command('someCommand', 'someValue');
+    
+    expect(command.commandType).toBe('someCommand');
+  });
+  it("constructor sets a value passed in as the 2nd argument", function(){
+    
+    let command = new Command('someCommand', 'someValue')
+    expect(command.value).toBe('someValue')
+  });
 
 });
