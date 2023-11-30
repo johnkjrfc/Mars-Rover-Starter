14d75b233eb56bfc9ad236c91c12d423c357e306
johnkjrfc <johnkjrfc@gmail.com>
2023-11-29 20:46:13 -0600

    test for rover module built, ready to write module

--- rover.js
+++ rover.js
@@ -1,5 +1,9 @@
 class Rover {
-   constructor
+   constructor(position, mode = "NORMAL", generatorWatts = 110) {
+   this.position = position;
+   this.mode = mode;
+   this.generatorWatts = generatorWatts;
+   }
 }
 
 module.exports = Rover;
